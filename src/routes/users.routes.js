edee8d5d103a5bdde5875b5ff76805069a0175b2
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-07-19 19:01:49 -0300

    protecting user access to localstorage

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const UsersController = require("../controllers/UsersController");
+const UsersValidate = require("../controllers/UserValidate");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidate = new UsersValidate();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.get("/validated", ensureAuthenticated, usersValidate.index);
 
 module.exports = usersRoutes;
